c36da8e1250c75bc3dfe2831d9a5975524a5ee0a
imevasoldi <imevasoldi@gmail.com>
2022-12-19 10:41:48 -0600

    final styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const cardElements = data.map(card => (
     <Card
     key={card.id}
-    card={card}    />
+    {...card}    />
     ))
     return (
       <div className="container">
@@ -21,15 +21,3 @@ function App() {
 }
 
 export default App
-
-// <Card
-//   key={card.id}
-//   title={card.title}
-//   price={card.price}
-//   url={card.coverImg}
-//   starurl="star.png"
-//   rating={card.stats.rating}
-//   views={card.stats.reviewCount}
-//   location={card.location}
-//   openSpots={card.openSpots}
-// />
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,23 +1,23 @@
 const Card = (props) => {
     let badgeText
-    if (props.card.openSpots === 0){
+    if (props.openSpots === 0){
         badgeText = "SOLD OUT"
-    }else if (props.card.location === "Online"){
+    }else if (props.location === "Online"){
 
         badgeText = "ONLINE"
     }
     return <div className="card">
         {/* {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} */}
         {badgeText && <div className="card--badge">{badgeText}</div>}
-        <img src={props.card.coverImg}/>
+        <img src={props.coverImg}/>
         <div className="stats--group">
-            <img style={{background: "green", width: "14px", height: "14px"}} src="star.png"/>
-            <span className="rating">{props.card.stats.rating}</span> 
-            <span className="views">({props.card.stats.reviewCount})·</span>
-            <span className="views">{props.card.location}</span>
+            <img style={{width: "14px", height: "14px"}} src="star.png"/>
+            <span className="rating">{props.stats.rating}</span> 
+            <span className="views">({props.stats.reviewCount}) ·</span>
+            <span className="views">{props.location}</span>
         </div>
-        <p>{props.card.title}</p>
-        <p><span className="bold">From ${props.card.price}</span> / person</p>
+        <p>{props.title}</p>
+        <p><span className="bold">From ${props.price}</span> / person</p>
     </div>
 }
 
